800224d8e644e5f00318b829c82234d14f4ce939
wei <wei3jiang1@gmail.com>
2015-02-23 01:54:21 +0800

    update ui.r

--- ui.R
+++ ui.R
@@ -1,20 +1,16 @@
 library(shiny)
-library(UsingR)
-data(galton)
-shinyServer(
-    function(input, output) {
-        x <- reactive(input$mu)
-        output$oid1 <- renderPrint({x()})
-        
-        
-        output$newHist <- renderPlot({
-            hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-            mu <- x()
-            lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-            mse <- mean((galton$child - mu)^2)
-            text(63, 150, paste("mu = ", mu))
-            text(63, 140, paste("MSE = ", round(mse, 2)))
-            output$oid2 <- renderPrint({round(mse,2)})
-        })
-    }
-)
+shinyUI(pageWithSidebar(
+    headerPanel("Child height plot and MSE"),
+    sidebarPanel(
+        sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05,),
+        p('Guess the mean (mu) and find out the MSE (mean squared error).'),
+        p('Data from galton http://www.bun.kyoto-u.ac.jp/~suchii/galton86.html')
+    ),
+    mainPanel(
+        h4('You selected '),
+        verbatimTextOutput("oid1"),
+        h4('The MSE is '),
+        verbatimTextOutput("oid2"),
+        plotOutput('newHist')
+    )
+))
